2ab7b57780dfa78a2e008cea5391f985dc05b720
Akash Mishra <tucosaurus@gmail.com>
2019-03-03 13:02:37 +0530

    feat - sample sum function for arrays

---
+++ arrays-slices/sum_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	numbers := [5]int{1, 2, 3, 4, 5}
+
+	output := Sum(numbers)
+	expected_output := 15
+
+	if output != expected_output {
+		t.Errorf("got '%d' want '%d' given '%v'", output, expected_output, numbers)
+	}
+}
